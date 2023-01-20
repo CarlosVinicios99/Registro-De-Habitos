c2df18291385bcd15e32766c61b78bbb9b080966
Carlos Vinicios <carlosvinicios@id.uff.br>
2023-01-20 09:34:15 -0300

    Salvamento do estado do formulario e automatizacao da criacao dos dias

--- habits/script.js
+++ habits/script.js
@@ -1,12 +1,28 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
-const data = {
-    run: ["01-01", "01-02", "01-06", "01-07", "01-08"],
-    takePills: ["01-03"],
-    journal: ["01-02"]
+const button = document.querySelector('header button')
 
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add(){
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia ja incluso")
+        return
+    }
+
+    alert("Dia adicionado com sucesso")
+    nlwSetup.addDay(today)
 }
 
+function save(){
+    localStorage.setItem("nlwSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("nlwSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
